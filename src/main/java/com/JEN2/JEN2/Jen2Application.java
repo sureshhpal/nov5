8cb13c3c58fa22a6ae29b9f6784bf3e848250571
suresh <sureshhpal@gmail.com>
2023-11-05 20:05:52 +0530

    first commit

---
+++ src/main/java/com/JEN2/JEN2/Jen2Application.java
@@ -0,0 +1,27 @@
+package com.JEN2.JEN2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class Jen2Application {
+	public static Logger logger=LoggerFactory.getLogger(Jen2Application.class);
+	
+	@javax.annotation.PostConstruct
+	public void init()
+	{
+		logger.info("Application started");;
+	}
+	
+	public static void main(String[] args) {
+		logger.info("Application Executed");;
+		SpringApplication.run(Jen2Application.class, args);
+		System.out.println("main function ");
+	}
+
+}
---
+++ src/test/java/com/JEN2/JEN2/Jen2ApplicationTests.java
@@ -0,0 +1,22 @@
+package com.JEN2.JEN2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class Jen2ApplicationTests {
+	public static Logger logger=LoggerFactory.getLogger(Jen2ApplicationTests.class);
+	@Test
+	void contextLoads() {
+		
+		logger.info("test case executed ");
+		assertEquals(true, true);
+	}
+
+}
